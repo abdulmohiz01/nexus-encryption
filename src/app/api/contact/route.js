a285f7efb9d2952824b0bda5546aa5a2b9ff3b03
Abdul Mohiz <skylark7768@gmail.com>
2024-08-04 23:36:43 +0500

    Email

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,47 +1,42 @@
 // Import necessary modules
-import nodemailer from 'nodemailer'
+import nodemailer from 'nodemailer';
 import { NextResponse } from 'next/server';
 
 // Define a function to handle POST requests
 export async function POST(request) {
   try {
     const data = await request.json();
-    // console.log(data);
     const { name, email, subject, message } = data;
+
+    // Create a transporter object using Gmail
     const transporter = nodemailer.createTransport({
-      service: 'Gmail',
+      service: 'gmail', // Note: Ensure this is lowercase 'gmail'
       auth: {
-        user: process.env.emailFrom,
-        pass: process.env.pass,
+        user: process.env.EMAIL_FROM, // Ensure these environment variables are set correctly
+        pass: process.env.PASS,
       },
     });
+
+    // Set up email data
     const mailOptions = {
-      from: process.env.emailFrom,
-      to: process.env.emailTo,
+      from: process.env.EMAIL_FROM,
+      to: process.env.EMAIL_TO, // Make sure this is set up in your environment variables
       subject: `${subject}`,
-      text: `Name: ${name} \nEmail: ${email}\n\nMessage: ${message}`,
+      text: `Name: ${name}\nEmail: ${email}\n\nMessage: ${message}`,
     };
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-        res.status(500).send('Internal Server Error');
-      }
-      else {
-        console.log('Email Sent: ' + info.response);
-      }
-    });
 
+    // Send email
+    await transporter.sendMail(mailOptions);
 
-    return NextResponse.json({ message: 'Data received successfully' }, { status: 200 });
+    return NextResponse.json({ message: 'Email sent successfully' }, { status: 200 });
   } catch (error) {
-    console.error(error);
+    console.error('Error sending email:', error);
     return NextResponse.json({ message: 'Error processing request' }, { status: 500 });
   }
 }
 
 // Define a function to handle GET requests (if needed)
 export async function GET(request) {
-  // Handle GET request (e.g., return some data)
   return NextResponse.json({ message: 'This is a GET request' }, { status: 200 });
 }
 
