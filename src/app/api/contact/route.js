568fb44e1f0713314a356a5f62db0c934a37387a
Abdul Mohiz <skylark7768@gmail.com>
2024-08-05 20:18:36 +0500

    nodemailer

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -8,8 +8,9 @@ export async function POST(request) {
     // console.log(data);
     console.log(process.env.emailTo, process.env.emailFrom);
     const { name, email, subject, message } = data;
-    const transporter = nodemailer.createTransport({
-      service: 'Gmail',
+    const transporter = nodemailer.createTransport( {
+      service: 'gmail',
+      host: "smtp.ethereal.email",
       auth: {
         user: process.env.emailFrom,
         pass: process.env.pass,
@@ -21,15 +22,8 @@ export async function POST(request) {
       subject: `${subject}`,
       text: `Name: ${name} \nEmail: ${email}\n\nMessage: ${message}`,
     };
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-        res.status(500).send('Internal Server Error');
-      }
-      else {
-        console.log('Email Sent: ' + info.response);
-      }
-    });
+
+    await transporter.sendMail(mailOptions);
 
 
     return NextResponse.json({ message: 'Data received successfully' }, { status: 200 });
@@ -38,7 +32,7 @@ export async function POST(request) {
     return NextResponse.json({ message: 'Error processing request' }, { status: 500 });
   }
 }
- 
-export async function GET(request) { 
+
+export async function GET(request) {
   return NextResponse.json({ message: 'This is a GET request' }, { status: 200 });
 }
