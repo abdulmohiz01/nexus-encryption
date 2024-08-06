4f1501f1be7f629962b37c5b001def40901c73b1
Abdul Mohiz <skylark7768@gmail.com>
2024-08-06 18:21:19 +0500

    Removed Blogs atm

--- src/components/header.js
+++ src/components/header.js
@@ -7,9 +7,9 @@ const Header = () => {
         <Link href={'/'}><Image src={'/nexus-1.png'} alt={'Nexus Encryption Logo'} priority width={550} height={50} /></Link>
       </div>
       
-      <div className="flex items-center justify-between md:px-12 py-3 border-y sm:px-2">
+      <div className="flex items-center justify-around md:m-auto  py-3 border-y sm:px-2">
         <Link href='/'>Home</Link>
-        <Link href='/blog'>Blogs</Link>
+        {/* <Link href='/blog'>Blogs</Link> */}
         <Link href='/ciphers'>Ciphers</Link>
         <Link href='/aboutUs'>About Us</Link>
         <Link href='/contact'>Contact</Link>
