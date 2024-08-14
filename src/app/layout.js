3baffda4678efb033f6e09d6de03a47374d53e1e
Abdul Mohiz <skylark7768@gmail.com>
2024-08-14 12:41:20 +0500

    gtm

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,9 @@ import Header from "./../components/header"
 import Footer from "./../components/footer"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
+import GoogleTagManager from "./../components/custom/googletagmanager"
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -26,14 +29,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <GoogleTagManager containerId={process.env.GTM_ID} />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6597722728082254"
+          crossorigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6597722728082254"
+          crossorigin="anonymous"></script>
 
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6597722728082254"
-          crossorigin="anonymous"></script>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6597722728082254"
-          crossorigin="anonymous"></script>
       </head>
 
+
       <body className={inter.className}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MRT3H7ZV"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <Header />
         {children}
         <Footer />
