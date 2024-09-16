6c41ecdb09fdfbbfd555bee1a7558cc08aec25bf
Abdul Mohiz <skylark7768@gmail.com>
2024-09-16 21:39:52 +0500

    remvoed g tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./../components/header"
 import Footer from "./../components/footer"
-// import { SpeedInsights } from "@vercel/speed-insights/next"
-// import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import Script from "next/script";
 
 
@@ -25,22 +25,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {/* All About Gooogle */}
-        {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6597722728082254"
-          crossorigin="anonymous"></script>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6597722728082254"
-          crossorigin="anonymous"></script>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.G_T}`}></script> */}
-        {/* <Script id='google-analytics'>
-          {
-            `window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', ${process.env.G_T});` 
-          }
-        </Script> */}
-      </head>
       <body className={inter.className}>
         <Header />
         {children}
